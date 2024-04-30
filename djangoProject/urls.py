cb7bcaec81218fdc4e7f9fd9ac83288cdfcb3daa
hz <155130148+NEWVH@users.noreply.github.com>
2024-04-30 19:43:33 +0900

    Update urls.py

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -21,4 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('main/', TemplateView.as_view(template_name='main.html'), name='main'),
+    path('hzz/', TemplateView.as_view(template_name='hzz.html'), name='hzz'),
 ]
